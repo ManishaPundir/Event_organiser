e30a0f3dd451c72d83e7aacaa35a88d0f83db10f
Manisha Chauhan <manisha.rynr@gmail.com>
2025-01-14 21:08:18 +0530

    Updated Database URI

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,8 @@
 const mongoose=require("mongoose");
 
 //const uri = 'mongodb://localhost:27017/MongoDB';
-mongoose.connect("mongodb://localhost:27017/youtubeRegistration",{
+//mongodb+srv://manisharynr:dLW96RQxdi1lWpe8@eventplanner.jinv2.mongodb.net/?retryWrites=true&w=majority&appName=eventplanner
+mongoose.connect("mongodb+srv://manisharynr:dLW96RQxdi1lWpe8@eventplanner.jinv2.mongodb.net/?retryWrites=true&w=majority&appName=eventplanner",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{
