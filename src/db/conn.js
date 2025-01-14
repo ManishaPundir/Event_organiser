eb645c5e09653606a249b01e7849b790019ca672
Manisha Chauhan <manisha.rynr@gmail.com>
2025-01-14 21:21:43 +0530

    Added db name in DB URI

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,9 @@
 const mongoose=require("mongoose");
 
 //const uri = 'mongodb://localhost:27017/MongoDB';
+// Original Local DB: mongodb://localhost:27017/youtubeRegistration
 //mongodb+srv://manisharynr:dLW96RQxdi1lWpe8@eventplanner.jinv2.mongodb.net/?retryWrites=true&w=majority&appName=eventplanner
-mongoose.connect("mongodb+srv://manisharynr:dLW96RQxdi1lWpe8@eventplanner.jinv2.mongodb.net/?retryWrites=true&w=majority&appName=eventplanner",{
+mongoose.connect("mongodb+srv://manisharynr:dLW96RQxdi1lWpe8@eventplanner.jinv2.mongodb.net/youtubeRegistration?retryWrites=true&w=majority&appName=eventplanner",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{
